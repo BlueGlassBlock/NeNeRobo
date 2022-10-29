a90bb264958eb3198db24ae7cb99ef4413b6e83b
ProgramRipper <programripper@foxmail.com>
2022-10-29 11:31:59 +0800

    :bug: pep use dark mode

--- modules/pep/render.py
+++ modules/pep/render.py
@@ -4,7 +4,9 @@ from launart import Launart
 
 async def PEP_to_image(pep: int) -> bytes:
     browser = Launart.current().get_interface(PlaywrightBrowser)
-    async with browser.page(viewport={"height": 800, "width": 1000}) as page:
+    async with browser.page(
+        color_scheme="dark", viewport={"height": 800, "width": 1000}
+    ) as page:
         await page.goto(
             f"https://peps.python.org/pep-{pep}/",
             timeout=5000,
